b46b67344712e9000bf8aeb32865bfafc7f3ac03
Anu CS <anu.cs@searshc.com>
2018-07-26 21:13:56 +0530

    connected router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { PageContainer } from './PageContainer.jsx';
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Home page      </div>
-    );
-  }
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ConnectedRouter } from 'connected-react-router'
+import routes from './routes'
+import { Link } from 'react-router-dom'
+const App = ({ history }) => {
+  return (
+    <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
+    <div>
+    <Link to="/hello" >Help</Link><br />
+    <Link to="/counter">Parts</Link><br />
+      { routes }
+      </div>
+    </ConnectedRouter>
+  )
 }
 
+App.propTypes = {
+  history: PropTypes.object,
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import PageContainer from './PageContainer.jsx';
-import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { createBrowserHistory } from 'history'
+import { routerMiddleware, connectRouter } from 'connected-react-router'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import rootReducer from './reducers'
+import createHistory from 'history/createBrowserHistory';
+//const history = createBrowserHistory()
+const history = createHistory({
+  basename: process.env.PUBLIC_URL
+});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  composeEnhancer(
+    applyMiddleware(
+      routerMiddleware(history),
+    ),
+  ),
+)
 
-ReactDOM.render(<PageContainer />, document.getElementById('root'));
-registerServiceWorker();
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <App history={history} />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('react-root')
+  )
+}
+
+render()
+
+// Hot reloading
+if (module.hot) {
+  // Reload components
+  module.hot.accept('./App', () => {
+    render()
+  })
+
+  // Reload reducers
+  module.hot.accept('./reducers', () => {
+    store.replaceReducer(connectRouter(history)(rootReducer))
+  })
+}
